0566210adf6a74cd30d48c5b008d205e95011c92
6thSence <d.pushkarskaya.pr@gmail.com>
2016-04-03 13:43:18 +0600

    postcss-nested + postcss-short

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,22 @@
 var gulp = require('gulp');
 var rename =  require('gulp-rename');
 var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var cssnano = require('cssnano');
+var nested = require('postcss-nested');
+var short = require('postcss-short');
 
 gulp.task('css', function () {
     var processors = [
-        autoprefixer({browsers: ['last 2 version']}),
-        cssnano(),
+    nested,
+    short,
     ];
     return gulp.src('./src/styles/stylesIn.css')
         .pipe(postcss(processors))
         .pipe(rename('styleOut.css'))
         .pipe(gulp.dest('./src/styles/'));
 });
+
+gulp.task('default', function() {
+    gulp.watch('src/styles/stylesIn.css', function() {
+        gulp.run('css');
+    })
+})
